d259b5ee812f52947bfc4a429552514ebcb936ce
RK <kanno.media808@gmail.com>
2024-03-21 11:12:56 -0700

    Create sample arrays.

--- PleaseDo/Preview Content/App/ContentView.swift
+++ PleaseDo/Preview Content/App/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var vm = ListVM()
+    
     var body: some View {
         NavigationStack {
             ZStack {
--- PleaseDo/Preview Content/ViewModels/ListVM.swift
+++ PleaseDo/Preview Content/ViewModels/ListVM.swift
@@ -9,8 +9,16 @@ import Foundation
 import Observation
 
 @Observable final class ListVM {
-    var items: [Item] = [
+    var todoItems: [Item] = [
         Item(id: "abc123", title: "Take A Break", description: "Make sure to take a break and rest your eyes", startDate: 1711043190, completedDate: nil, status: .todo, priority: .medium),
         Item(id: "123abc", title: "Have A Snack", description: "Small snacks throughout the day are a great way to increase blood sugar for people with hypoglycemia", startDate: 1711043200, completedDate: nil, status: .todo, priority: .low)
+    ]
+    
+    var inProgressItems: [Item] = [
+        Item(id: "xyz123", title: "Be Grateful", description: "Taking time everyday to be grateful is a habit that can dramatically change your life.", startDate: 1711043500, completedDate: nil, status: .inProgress, priority: .high)
+    ]
+    
+    var doneItems: [Item] = [
+        
     ]
 }
