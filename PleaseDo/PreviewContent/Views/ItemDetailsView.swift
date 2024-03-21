ece7da0de25979edf13cfbc7362d903c34206a3d
RK <kanno.media808@gmail.com>
2024-03-21 12:11:10 -0700

    Add ItemDetailsView to project.

---
+++ PleaseDo/PreviewContent/Views/ItemDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemDetailsView.swift
+//  PleaseDo
+//
+//  Created by R K on 3/21/24.
+//
+
+import SwiftUI
+
+struct ItemDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemDetailsView()
+}
