db4bdf181df175a5d337c4a2fd75d8b1e2a5bef8
RK <kanno.media808@gmail.com>
2024-03-21 17:52:02 -0700

    Add NavigationManager to project.

---
+++ PleaseDo/PreviewContent/ViewModels/NavigationManager.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationManager.swift
+//  PleaseDo
+//
+//  Created by R K on 3/21/24.
+//
+
+import SwiftUI
+import Observation
+
+@Observable final class NavigationManager {
+    var path = NavigationPath()
+    
+    
+}
