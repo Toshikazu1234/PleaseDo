c6407463db262a73e6ad6f31702e4fbbf40d1e07
RK <kanno.media808@gmail.com>
2024-03-23 17:53:25 -0700

    Add Item initializers.

--- PleaseDo/PreviewContent/Models/Item.swift
+++ PleaseDo/PreviewContent/Models/Item.swift
@@ -6,18 +6,43 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 struct Item: Identifiable, Equatable, Hashable {
     let id: String
     var title: String
     var description: String
-    let startDate: TimeInterval
-    let completedDate: TimeInterval?
+    var startDate: Date = .now
     var status: Status
     var priority: Priority
+    var isComplete: Bool = false
     
     static func == (lhs: Item, rhs: Item) -> Bool {
         return lhs.id == rhs.id
+    }
+    
+    enum DBKeys: String {
+        case id, title, description, startDate, status, priority, isComplete
+    }
+    
+    init(id: String, title: String, description: String, status: Status, priority: Priority) {
+        self.id = id
+        self.title = title
+        self.description = description
+        self.status = status
+        self.priority = priority
+    }
+    
+    init(data: [String: Any]) {
+        self.id = data[DBKeys.id.rawValue] as! String
+        self.title = data[DBKeys.title.rawValue] as! String
+        self.description = data[DBKeys.description.rawValue] as! String
+        self.startDate = (data[DBKeys.startDate.rawValue] as! Timestamp).dateValue()
+        let statusString = data[DBKeys.status.rawValue] as! String
+        self.status = Status(statusString)
+        let priorityString = data[DBKeys.priority.rawValue] as! String
+        self.priority = Priority(priorityString)
+        self.isComplete = data[DBKeys.isComplete.rawValue] as! Bool
     }
 }
 
@@ -25,16 +50,36 @@ enum Status: String, CaseIterable {
     case todo = "To Do"
     case inProgress = "In Progress"
     case done = "Done"
+    case unknown = "Unknown"
+    
+    init(_ str: String) {
+        self = switch str {
+        case "To Do": .todo
+        case "In Progress": .inProgress
+        case "Done": .done
+        default: .unknown
+        }
+    }
 }
 
 enum Priority: String, CaseIterable {
-    case low, medium, high
+    case low, medium, high, unknown
+    
+    init(_ str: String) {
+        self = switch str {
+        case "low": .low
+        case "medium": .medium
+        case "high": .high
+        default: .unknown
+        }
+    }
     
     var color: Color {
         return switch self {
         case .low: .blue
         case .medium: .orange
         case .high: .red
+        case .unknown: .gray
         }
     }
     
@@ -43,6 +88,7 @@ enum Priority: String, CaseIterable {
         case .low: "circle"
         case .medium: "triangle"
         case .high: "exclamationmark.circle"
+        case .unknown: ""
         }
     }
 }
