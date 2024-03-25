355757281a121feb859488bc0740e066bac7d11a
RK <kanno.media808@gmail.com>
2024-03-24 18:48:19 -0700

    Add NewItemVM to project.

---
+++ PleaseDo/PreviewContent/ViewModels/NewItemVM.swift
@@ -0,0 +1,16 @@
+//
+//  NewItemVM.swift
+//  PleaseDo
+//
+//  Created by R K on 3/24/24.
+//
+
+import Foundation
+import Observation
+
+@Observable final class NewItemVM {
+    
+    init() {
+        
+    }
+}
