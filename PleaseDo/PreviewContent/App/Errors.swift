33edaeba6750c692f9310e6c4e83576dc8e7b126
RK <kanno.media808@gmail.com>
2024-03-24 19:01:43 -0700

    Add custom Errors enum.

---
+++ PleaseDo/PreviewContent/App/Errors.swift
@@ -0,0 +1,12 @@
+//
+//  Errors.swift
+//  PleaseDo
+//
+//  Created by R K on 3/24/24.
+//
+
+import Foundation
+
+enum Errors: String, Error {
+    case noCurrentUser = "Error getting current user."
+}
